38f12d98b16f9cb0675760a79e0c91907b01f303
ayeon <dkdus591@gmail.com>
2025-05-30 18:09:18 +0900

    env error fix

--- config/database.js
+++ config/database.js
@@ -4,18 +4,22 @@ const sql= require('pg');
 const pool = new sql.Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
+  database: String(process.env.DB_DATABASE),
   password: String(process.env.DB_PASSWORD),
   port: Number(process.env.DB_PORT),
 });
 
 
 const connectToDb = async () => {
+  let client;
   try {
-    await pool.connect();
+    client = await pool.connect();
     console.log('Database connected successfully');
   } catch (err) {
     console.error('Database connection error:', err);
+    throw err;
+  } finally {
+    client.release();
   }
 };
 
