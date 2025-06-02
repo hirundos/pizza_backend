b546c3657ccd3dc68243d7818a6b556a45a7b266
ayeon <dkdus591@gmail.com>
2025-06-02 10:26:21 +0900

    add api_spec.md

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-  origin: process.env.CLIENT_ORIGIN, 
+  //origin: process.env.CLIENT_ORIGIN, 
+  origin: true,
   credentials: true
 }));
 
